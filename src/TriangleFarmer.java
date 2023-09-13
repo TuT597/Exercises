5c20c5fb44d16ee3ee7acf1d70c635331f37bad0
Tuyan T <tuyantat@gmail.com>
2023-09-13 10:51:23 +0200

    Update TriangleFarmer.java

--- src/TriangleFarmer.java
+++ src/TriangleFarmer.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class TriangleFarmer {
-    public static void main (String[] args) {
+    public static void main (String[] args) { 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Time to figure out the area of your triangle.\nWhat is the base of your triangle?");
         int base = scanner.nextInt();
